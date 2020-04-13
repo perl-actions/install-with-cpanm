f91d1725c3713a1e60b3800d2245499efe2c3c6e
Nicolas R <nicolas@atoomic.org>
2020-04-13 13:32:59 -0600

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# install-cpanm
-GitHub action to install App::cpanm
+[![Actions Status](https://github.com/perl-actions/install-cpanm/workflows/check/badge.svg)](https://github.com/perl-actions/install-cpanm/actions)
+
+# install-cpm
+
+GitHub action to install App::cpanminus
+
+This action installs 'cpnaminus' as root so you can then use it in your workflow.
+
+## Inputs
+
+none
+
+## Outputs
+
+none
+
+## Example usage
+
+```
+uses: perl-actions/install-cpanm@v1.0
+run: |
+   sudo cpanm Module::To::Install
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const tc = require('@actions/tool-cache');
+const exec = require('@actions/exec');
+
+async function action() {
+  const cpanm = await tc.downloadTool('https://cpanmin.us');
+  core.setOutput("cpanminus", cpanm);
+  await exec.exec( 'sudo', [ 'perl', cpanm, 'App::cpanminus'] );
+  return;
+}
+
+// Call action
+(async() => {
+  try {
+  	await action();	
+  } catch (error) {
+  	core.setFailed(error.message);	
+  }
+})();
