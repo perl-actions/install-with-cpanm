b4dd777227cb3e5c8657a39f62ab3f466bb208cb
Nicolas R <cpan@atoomic.org>
2020-04-17 09:35:07 -0600

    prettier

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const tc = require('@actions/tool-cache');
-const exec = require('@actions/exec');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const tc = require("@actions/tool-cache");
+const exec = require("@actions/exec");
 
 async function action() {
-  const cpanm = await tc.downloadTool('https://cpanmin.us');
+  const cpanm = await tc.downloadTool("https://cpanmin.us");
   core.setOutput("cpanminus", cpanm);
-  await exec.exec( 'sudo', [ 'perl', cpanm, 'App::cpanminus'] );
+  await exec.exec("sudo", ["perl", cpanm, "App::cpanminus"]);
   return;
 }
 
 // Call action
-(async() => {
+(async () => {
   try {
-  	await action();	
+    await action();
   } catch (error) {
-  	core.setFailed(error.message);	
+    core.setFailed(error.message);
   }
 })();
